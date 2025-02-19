f0ac9581660b08539adfce2ba7cb226533c66c86
mfoteino <memosfoteinopoulos@gmail.com>
2025-02-19 11:12:48 +0200

    removed client addition from client.go

--- client.go
+++ client.go
@@ -35,14 +35,6 @@ func (s *ChatServer) handleClient(conn net.Conn) {
 		return
 	}
 
-	// Create a new Client instance with the connection and chosen name.
-	client := &Client{conn: conn, name: name}
-
-	// Register the client in the server's clients map.
-	s.mu.Lock()
-	s.clients[conn] = client
-	s.mu.Unlock()
-
 	// Send the stored chat history to the new client.
 	s.sendHistory(conn)
 
